7526df6af148ef6d15313234279cb492e5676726
kateizsong <kjsong@ad.unc.edu>
2025-03-03 00:36:38 -0500

    Basketball Games

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,68 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var games: [Game]
+}
+
+struct Game: Codable {
+    var isHomeGame: Bool
+    var date: String
+    var score: Score
+    var team: String
+    var opponent: String
+    var id: Int
+    
+}
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+    @State private var games = [Game]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(games, id: \.id) { game in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(game.team) vs \(game.opponent)")
+                        Text("\(game.date)")
+                            .font(.caption)
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("\(game.score.unc) - \(game.score.opponent)")
+                        Text(game.isHomeGame ? "Home": "Away")
+                    }
+                }
+            }
+            
+            .navigationTitle("UNC Basketball")
+            .task {
+                await loadData()
+            }
+            
+            
         }
-        .padding()
+    }
+        
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Game].self, from: data) {
+                games = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
